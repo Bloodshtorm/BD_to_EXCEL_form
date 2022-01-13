38b290676ddc64e58f88eebc3925165eade8eeec
https://aksionik@bitbucket.org/aksionik/gkh_samara.git <ActanonVerba21>
2022-01-13 10:04:53 +0400

    next

--- BD_to_EXCEL_form/Form1.cs
+++ BD_to_EXCEL_form/Form1.cs
@@ -14,6 +14,7 @@ using Npgsql;
 using DocumentFormat.OpenXml;
 using ClosedXML.Excel;
 using DotNetDBF;
+using System.Data.OleDb;
 
 namespace BD_to_EXCEL_form
 {
@@ -494,101 +495,105 @@ namespace BD_to_EXCEL_form
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            try
+            DataTable selDt = new DataTable();
+            /*NpgsqlDataAdapter sda = new NpgsqlDataAdapter($@"select acc_num ACC_CODE, 
+                                        b4fa.street_name as STREET,
+                                        b4f.shortname as STR_TYPE,
+                                        b4fa.house as HOUSE,
+                                        croom_num as FLAT_NUM,
+                                        LTRIM(own.name, ' ') as MASTER,
+                                        to_char(rpa.open_date, 'dd.mm.yyyy'),
+                                        to_char(rpa.close_date, 'dd.mm.yyyy'),
+                                        null AS PHONE,
+                                        case 
+                                        	when gr.ownership_type = 10 then 'Частная' 
+                                        	when gr.ownership_type = 30 then 'Муниципальная' 
+                                        	when gr.ownership_type = 40 then 'Государственная'
+                                        	when gr.ownership_type = 50 then 'Коммерческая' 
+                                        	when gr.ownership_type = 60 then 'Смешанная' 
+                                        	when gr.ownership_type = 80 then 'Федеральная'
+                                        	when gr.ownership_type = 90 then 'Областная' 
+                                        	else 'Не указано' 
+                                        end as OWNERSHIP,
+                                        case 
+                                        	when gr.type = 10 and gr.is_communal = true then 'Коммунальное помещение'
+                                        	when gr.type = 10 then 'Жилое помещение'
+                                        	when gr.type = 20 then 'Нежилое помещение' 
+                                        	else 'Не указано' 
+                                        end as HABIT_TYPE,
+                                        area_mkd as TOTAL_SQ,
+                                        area_living_owned as LIVING_SQ,
+                                        null AS LODGER_CNT
+                                        from regop_pers_acc rpa
+                                        join gkh_room gr on gr.id=rpa.room_id
+                                        join regop_pers_acc_owner own on rpa.acc_owner_id = own.id and rpa.state_id = 804
+                                        join gkh_reality_object ro on ro.id = gr.ro_id
+                                        join b4_fias_address b4fa on b4fa.id= ro.fias_address_id
+                                        join b4_fias b4f on b4f.aoguid = b4fa.street_guid and b4f.actstatus=1
+                                        where municipality_id=4808
+                                        order by 2,4,5,6", con);
+            sda.Fill(selDt);*/
+
+            log("Количество записей: " + selDt.Rows.Count);
+
+            for (int i = 0; i < selDt.Rows.Count+1; i++)
             {
-                string dataname = date();
-                using (Stream fos = File.Open(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + dataname + ".dbf", FileMode.OpenOrCreate, FileAccess.ReadWrite))
-                using (var writer = new DBFWriter())
+                Console.WriteLine("Начинаю файл " + i);
+                OleDbConnection conn = new OleDbConnection();
+                //log(System.IO.Path.GetDirectoryName(Application.ExecutablePath));
+                //log(System.IO.Path.GetDirectoryName(Application.StartupPath));
+                //log(System.IO.Path.GetDirectoryName(Application.LocalUserAppDataPath));
+
+                conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+Application.StartupPath + ";Extended Properties=dBASE IV;User ID=Admin;";
+                conn.Open();
+                OleDbCommand comm = conn.CreateCommand();
+                comm.CommandText = "DELETE * FROM SHABLON"; 
+                comm.ExecuteNonQuery();
+                conn.Close();
+                log(System.IO.Path.GetDirectoryName(Application.ExecutablePath)+@"\SHABLON.dbf");
+
+                FileInfo fn = new FileInfo(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"\SHABLON.dbf");
+                fn.CopyTo(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"\Data\SHABLON.dbf", true);
+                conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + "\\Data;Extended Properties=dBASE IV;User ID=Admin;";
+                conn.Open();
+                comm.CommandText = "INSERT INTO SHABLON VALUES('34','Региональный','6806','Октябрьское','с','ул','Ленина','57','','12','2018','8','капитальный ремонт','капитальный ремонт','кв.м',7.400,23.02,123.20,6563.54,6663.25,5305.40,3)";
+                comm.ExecuteNonQuery();
+                /*while (selDt.Rows[i][0].ToString() == mainDt.Rows[ji][0].ToString())
                 {
-                    writer.CharEncoding = Encoding.GetEncoding(866);
-                    //writer.Signature = DBFSigniture.DBase3;
-                    writer.LanguageDriver = 0x26; // кодировка 866
-                    
-
-                    var field1 = new DBFField("Id", NativeDbType.Long);
-                    var field2 = new DBFField("DATE", NativeDbType.Date, );
-                    var field3 = new DBFField("POSNAME", NativeDbType.Long);
-                    var field4 = new DBFField("POSCODE", NativeDbType.Long);
-
-                    writer.Fields = new[] {field1, field2, field3, field4};
-                    writer.AddRecord("1", "2021-10-20", "тест1", "тест2");
-                    /*foreach (var item in items)
+                    try
                     {
-                        writer.AddRecord(
-                            // добавляем поля в набор
-                            );
-                    }*/
-                    writer.Write(fos);
-                }
-            }
-            catch (Exception ex)
-            {
-                log(ex.Message);
-            }
-            finally
-            {
-                //ndr.Close();
-                //if (con.State == ConnectionState.Open)con.Close();
-                // con.Dispose();
-            }
-            /*
-            try
-            {
-                strok_csv = "";
-                foreach (string s in headerTable)
-                {
-                    strok_csv += @"""" + s.ToString() + @""";";
-                }
-                log("Заполняем заголовки");
-                file.WriteLine(strok_csv);
-
-                string temp_strok = "";
-                if (ndr.HasRows)
-                {
-                    while (ndr.Read())
-                    {
-                        strok_csv = "";
-                        for (int x = 0; x < ndr.FieldCount; x++)
+                        //comm.CommandText = "INSERT INTO SHABLON VALUES('34','Региональный','6806','Октябрьское','с','ул','Ленина','57','','12','2018','8','капитальный ремонт','капитальный ремонт','кв.м',7.400,23.02,123.20,6563.54,6663.25,5305.40,3)";
+                        comm.CommandText = "INSERT INTO SHABLON VALUES('" + mainDt.Rows[ji][0].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][1].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][2].ToString()
+                            + "'" + delimeter + "'" + mainDt.Rows[ji][3].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][4].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][5].ToString() + "'"
+                            + delimeter + "'" + mainDt.Rows[ji][6].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][7].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][8].ToString() + "'"
+                            + delimeter + "'" + mainDt.Rows[ji][9].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][10].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][11].ToString() + "'"
+                            + delimeter + "'" + mainDt.Rows[ji][12].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][13].ToString() + "'" + delimeter + "'" + mainDt.Rows[ji][14].ToString() + "'"
+                            + delimeter + mainDt.Rows[ji][15].ToString() + delimeter
+                            + Decimal.Round(Convert.ToDecimal(mainDt.Rows[ji][16].ToString()), 2).ToString().Replace(',', '.') + delimeter
+                            + Decimal.Round(Convert.ToDecimal(mainDt.Rows[ji][17].ToString()), 2).ToString().Replace(',', '.') + delimeter
+                            + Decimal.Round(Convert.ToDecimal(mainDt.Rows[ji][18].ToString()), 2).ToString().Replace(',', '.') + delimeter
+                            + Decimal.Round(Convert.ToDecimal(mainDt.Rows[ji][19].ToString()), 2).ToString().Replace(',', '.') + delimeter
+                            + Decimal.Round(Convert.ToDecimal(mainDt.Rows[ji][20].ToString()), 2).ToString().Replace(',', '.') + delimeter
+                            + Convert.ToInt32(mainDt.Rows[ji][21].ToString()).ToString() + ")";
+                        comm.ExecuteNonQuery();
+                        if (ji < mainDt.Rows.Count - 1)
                         {
-                            try
-                            {
-                                temp_strok = ndr.GetValue(x).ToString().Replace(@"/n", "").Replace(@"/r", "");
-                                //strok_csv += @"""" + temp_strok + @""";";
-                                strok_csv += temp_strok + @";";
-                            }
-                            catch (System.InvalidCastException)
-                            {
-                                //log(ice.Message);
-                                //log("Вероятнее всего проблема с преобразованием даты \"infinity\"");
-                                temp_strok = "infinity";
-                                //strok_csv += @"""" + temp_strok + @""";";
-                                strok_csv += temp_strok + @";";
-                            }
-
+                            ji++;
                         }
-                        file.WriteLine(strok_csv);
+                        else
+                        {
+                            break;
+                        }
                     }
-                }
-                else
-                {
-                    log("Не обнаружены строки для записи в csv");
-                }
-                log("Генерация csv файла, запись данных: ГОТОВО");
-                //file.Close();
-                //ndr.Close();
-                //con.Dispose();
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        lf.WriteLine(e.Message);
+                    }
+                }*/
+                conn.Close();
+                //File.Move(@"D:\temp\sobits\1234\SHABLON.dbf", @"D:\temp\sobits\1234\" + selDt.Rows[i][0].ToString() + "18080000.dbf");
             }
-            catch (Exception ex)
-            {
-                log(ex.Message);
-            }
-            finally
-            {
-                ndr.Close();
-                file.Close();
-                //if (con.State == ConnectionState.Open)con.Close();
-                // con.Dispose();
-            }*/
         }
     }
 }
